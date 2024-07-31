5bc240db9f60d58acafb2507e86f62ab8500f528
Flo <90013108+FlorianAdamczyk@users.noreply.github.com>
2024-07-31 22:27:04 +0200

    test für klausur

---
+++ Klausur/AdamczykFlorian Test vor der Klausur.R
@@ -0,0 +1,22 @@
+# Adamczyk Florian 8105234
+
+# Das ist nur ein Test ob das hochladen funktioniert und ob mein R Programm tut.
+
+# Funktion, die die Summe von zwei Zahlen berechnet
+summe <- function(a, b) {
+  return(a + b)
+}
+
+# Test der Funktion
+
+summe(3, 4) # 7
+summe(5, 5) # 10
+summe(0, 0) # 0
+summe(-1, 1) # 0
+summe(-1, -1) # -2
+summe(-6, 4) # -2
+summe(37, 42) # 79
+summe(100, 100) # 200
+
+# test fertig.
+
