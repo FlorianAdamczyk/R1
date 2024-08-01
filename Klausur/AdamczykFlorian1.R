7e79f8584c976b4e5c2408ef95014ee738f3a280
Flo <90013108+FlorianAdamczyk@users.noreply.github.com>
2024-08-01 09:12:49 +0200

    klausur aufgabenstellung upload

--- Klausur/AdamczykFlorian1.R
+++ Klausur/AdamczykFlorian1.R
@@ -1,2 +1,13 @@
 # Adamczyk Florian 8105234
 
+# Aufgabe 1
+# a)
+# Lesen Sie mit read.csv die Daten in R ein und kontrollieren Sie Struktur und Inhalt der eingelesenen Daten!
+# Lesen Sie die CSV-Datei ein
+daten <- read.csv("Buecherwerte.csv")
+
+# Überprüfen Sie die Struktur der eingelesenen Daten
+str(daten)
+
+# Überprüfen Sie den Inhalt der eingelesenen Daten
+head(daten)
