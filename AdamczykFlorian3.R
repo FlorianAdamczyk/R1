08073272ca8ec7a42cc0f18e34ffe36d3a9882ac
Flo <90013108+FlorianAdamczyk@users.noreply.github.com>
2024-08-01 10:10:03 +0200

    bis aufgabe 4 gekommen

--- AdamczykFlorian3.R
+++ AdamczykFlorian3.R
@@ -1,2 +1,24 @@
 # Adamczyk Florian 8105234
 
+# Aufgabe 3
+# Ein Produktionsverfahren f¨ur ein Werkst¨uck liefere angeblich h¨ochstens 3 % Ausschuss. Es werden 100 unabh¨angig voneinander damit hergestellte Werkst¨ucke der Qualit¨atskontrolle unterzogen. F¨unf der 100 St¨ucke ergeben sich zu Ausschuss.
+
+# a) 
+# 5% Ausschuss wären zu erwarten bei 5 von 100 in der Stichprobe.
+
+# b)
+# Spricht die Beobachtung von f¨unf St¨ucken Ausschuss gegen die angebliche Schwelle von 3 %? Begr¨unden Sie Ihre Antwort und ziehen Sie dazu die Wahrscheinlichkeit heran, mit der ein mindestens so schlechtes Ergebnis in dem Produktionsverfahren zu beobachten w¨are, wenn die Ausschussquote tats¨achlich 3 % betr¨uge!
+
+# Wir können die Wahrscheinlichkeit berechnen, dass 5 oder mehr Ausschussstücke in einer Stichprobe von 100 Stücken vorkommen, wenn die Ausschussquote tatsächlich 3% beträgt.
+# Das ist eine Binomialverteilung mit n=100 und p=0.03
+# Wir berechnen die Wahrscheinlichkeit für 0 bis 4 Ausschussstücke und ziehen das von 1 ab, um die Wahrscheinlichkeit für 5 oder mehr Ausschussstücke zu erhalten.
+
+1-pbinom(4, 100, 0.03, lower.tail = FALSE)
+
+# es ist relativ wahrscheinlich, dass 5 oder mehr Ausschussstücke in einer Stichprobe von 100 Stücken vorkommen, wenn die Ausschussquote tatsächlich 3% beträgt.
+# das liegt einfach an der Wahrscheinlichkeit, es sind nicht immer genau 3 und 5 ist jetzt nicht so viel mehr als 3, dass es unwahrscheinlich wäre.
+# das liegt an der geringen Anzahl an Stücken in der Stichprobe, dass es so wahrscheinlich ist, dass 5 oder mehr Ausschussstücke vorkommen.
+# wenn man die Stichprobe vergrößern würde, würde es unwahrscheinlicher werden, dass 5 oder mehr Ausschussstücke vorkommen.
+
+1-pbinom(4, 500, 0.03, lower.tail = FALSE)
+ # deutlich unwahrscheinlicher.
--- AdamczykFlorian4.R
+++ AdamczykFlorian4.R
@@ -1,2 +1,43 @@
 # Adamczyk Florian 8105234
 
+# Aufgabe 4
+# 4x4 Matrix mit komischen Brüchen
+
+# a)
+# Ermitteln Sie – ohne R! – die (sehr einfache) Formel, mit der die Elemente Hi,j obiger Matrix in Abh¨angigkeit ihrer Zeilen- und Spaltenindices i bzw. j berechnet werden!
+
+# H_{i,j} = \frac{1}{i+j-1}
+# nehme an i und j beginnen beide bei 1 und zählen bis 4. Ich nummeriere die Zeilen und spalten also beginnend mit 1.
+
+# b)
+# Verwenden Sie lediglich die Funktionen matrix, seq (bzw. ”:“), rep, col und/oder row sowie elementare Arithmetik, um die obige (4 × 4)-Matrix mit R zu reproduzieren (ohne dass Sie diese Matrix ”einfach eintippen“)!
+
+# erstelle eine 4x4 Matrix mit 0en
+matrix <- matrix(0, nrow = 4, ncol = 4)
+matrix # wollte nachschauen ob das so geklapt hat
+# fülle die Matrix mit den Werten
+matrix[1:4,1:4] <- 1/(row(matrix) + col(matrix) - 1)
+matrix
+1/7
+1/3
+1/6
+
+# joa sollte so passen! ;)
+
+# c)
+# Verallgemeinern Sie nun Ihren Programmcode zu einer Funktion mit einem Argument n, die zu diesem n die entsprechende (n × n)-Matrix erzeugt!
+
+createStrangeMatrix <- function(n) {
+  # erstelle eine nxn Matrix mit 0en
+  matrix <- matrix(0, nrow = n, ncol = n)
+  # fülle die Matrix mit den Werten
+  matrix[1:n,1:n] <- 1/(row(matrix) + col(matrix) - 1)
+  return(matrix)
+}
+
+# teste die Funktion
+createStrangeMatrix(4)
+createStrangeMatrix(7)
+1/13
+
+# sollte auch passen!
